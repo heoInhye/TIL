be8c97de0cb8f99e6b08205b68bf6dc46d395129
christy <pepupx2@naver.com>
2022-05-05 15:36:20 +0900

    first commit

---
+++ src/test/java/com/study/TilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
